e73b9999a178e8a87d0868a8e51681facc87c072
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-17 11:31:17 -0300

    Exercicios bloco 4.1

---
+++ 4.1/exercicio1.js
@@ -0,0 +1,31 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+//Adição (a + b)
+//Subtração (a - b)
+//Multiplicação (a * b)
+//Divisão (a / b)
+//Módulo (a % b)
+//
+
+
+let a = 1;
+let b = 2;
+
+let soma = a + b; {
+  console.log(soma);
+}
+
+let subtracao = a - b; {
+  console.log(subtracao);
+}
+
+let multiplicacao = a * b; {
+  console.log(multiplicacao);
+}
+
+let divisao = a / b; {
+  console.log(divisao);
+}
+
+let modulo = a % b; {
+  console.log(modulo);
+}
---
+++ 4.1/exercicio3.js
@@ -0,0 +1,21 @@
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+
+
+let a = 1;
+let b = 2;
+let c = 5;
+
+if( a>b && a>c ) {
+  console.log('O maior valor é: ' + a + ', valor de a.');
+}
+
+else if (b>a && b>c) {
+  console.log('O maior valor é: ' + b + ', valor de b.');
+}
+
+else {
+  console.log ('O maior valor é: ' + c + ', valor de c.');
+}
+
+
